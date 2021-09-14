0187ec1c84d5418648e7190c0caf239b48fd3bec
Volkan Benli <volkan.benli@lighthouselabs.com>
2021-09-14 04:00:38 -0400

    Linting

--- head.js
+++ head.js
@@ -8,7 +8,7 @@ const assertEqual = function(actual, expected) {
 
 const head = function(arr) {
   return (arr.length >= 1) ? arr[0] : undefined;
-}
+};
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
