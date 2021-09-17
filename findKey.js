d1fff4fb3c72e9dc03720de3e1522c7af66f09ee
Volkan Benli <volkan.benli@lighthouselabs.com>
2021-09-17 03:50:59 -0400

    Adds findKey function tests

--- findKey.js
+++ findKey.js
@@ -15,3 +15,23 @@ const findKey = (obj, callback) => {
   return undefined;
 };
 
+// Test cases
+let result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+assertEqual(result1, "noma");
+
+let result2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars < 2) // => "noma"
+assertEqual(result2, "Blue Hill");
