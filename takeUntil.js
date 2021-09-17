8cd5b4fb057dcb8572cb6eff324312853fe0c033
Volkan Benli <volkan.benli@lighthouselabs.com>
2021-09-17 03:42:14 -0400

    Adds takeUntil function tests

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,23 @@
+const eqArrays = function (arr1, arr2) {
+  res = true;
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      res = (arr1[i] !== arr2[i]) ? false : true;
+    }
+  } else {
+    res = false;
+  }
+  return res;
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} === ${arr2}`);
+  }
+}
+
 const takeUntil = (arr, callback) => {
   let result = [];
   for (let i = 0; i < arr.length; i++) {
@@ -8,12 +28,11 @@ const takeUntil = (arr, callback) => {
   }
 };
 
+// Test cases
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
-
-console.log('---');
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
