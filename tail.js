c81e532167e9a35e19fa4f1aad0b14bd541e8e9b
Volkan Benli <volkan.benli@lighthouselabs.com>
2021-09-20 19:37:00 -0400

    Separate tail tests

--- tail.js
+++ tail.js
@@ -1,27 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} === ${expected}`);
-  }
-};
-
 const tail = function(arr) {
   return arr.slice(1);
 };
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-// Test Case 2: Check the original array 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-// Test Case 3: Check empty array 
-const empty = [];
-const result3 = tail(empty);
-assertEqual(result3.length, 0); 
+module.exports = tail;
