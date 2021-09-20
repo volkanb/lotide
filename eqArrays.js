fb7101355b4546811cd80b94d0fe517a357b9c30
Volkan Benli <volkan.benli@lighthouselabs.com>
2021-09-20 19:40:08 -0400

    Separate eqArrays tests

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} === ${expected}`);
-  }
-};
-
 const eqArrays = function (arr1, arr2) {
   res = true;
   if (arr1.length === arr2.length) {
@@ -18,10 +10,4 @@ const eqArrays = function (arr1, arr2) {
   return res;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArrays;
