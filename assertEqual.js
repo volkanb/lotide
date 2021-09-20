50d5fe2f25511d7a81c14e4e208f6e2209a849a8
Volkan Benli <volkan.benli@lighthouselabs.com>
2021-09-20 19:31:54 -0400

    Seperate assertEqual tests

--- assertEqual.js
+++ assertEqual.js
@@ -7,8 +7,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Bootcamp", "Bootcamp");
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 2);
+module.exports = assertEqual;
