2118e64964dacffd4cd0d947e567582d68f296e7
Volkan Benli <volkan.benli@lighthouselabs.com>
2021-09-20 19:32:04 -0400

    Seperate head tests

--- head.js
+++ head.js
@@ -1,15 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} === ${expected}`);
-  }
-};
-
 const head = function(arr) {
   return (arr.length >= 1) ? arr[0] : undefined;
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
+module.exports = head;
