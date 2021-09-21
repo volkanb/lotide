c333a6b262a54f97904570698a1e73c41e14ac54
Volkan Benli <volkan.benli@lighthouselabs.com>
2021-09-20 21:06:18 -0400

    Integrate mocha and chai for tail tests

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,18 +1,18 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+describe("#tail", () => {
+  it("should return ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ['Lighthouse', 'Labs']); 
+  });
 
-// Test Case 2: Check the original array 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+  it("should not modify the original array", () => {
+    const input = ['Hello', 'Lighthouse', 'Labs'];
+    tail(input);
+    assert.deepEqual(input, ['Hello', 'Lighthouse', 'Labs']); 
+  });
 
-// Test Case 3: Check empty array 
-const empty = [];
-const result3 = tail(empty);
-assertEqual(result3.length, 0); 
+  it("should return [] for []", () => {
+    assert.deepEqual(tail([]), []); 
+  });
+});
