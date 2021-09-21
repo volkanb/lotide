b594ed826639ef4327988fb139c3cd9db0efdcb9
Volkan Benli <volkan.benli@lighthouselabs.com>
2021-09-20 23:39:07 -0400

    WIP creates index.js for all module exports

---
+++ index.js
@@ -0,0 +1,16 @@
+// index.js
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const eqArrays = require('./eqArrays');
+const assertEqual = require('./assertEqual');
+const assertArrayEqual = require('./assertArraysEqual');
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  eqArrays: eqArrays,
+  assertEqual: assertEqual,
+  assertArrayEqual: assertArrayEqual
+};
